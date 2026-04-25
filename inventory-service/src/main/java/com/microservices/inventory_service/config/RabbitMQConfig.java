b66ba581a0e7ed6a03363bac4eab14031eb7632f
minhhoang <vqminhhoang2445@gmail.com>
2026-04-25 14:05:01 +0700

    containerize 3 services + rabbitmq

--- inventory-service/src/main/java/com/microservices/inventory_service/config/RabbitMQConfig.java
+++ inventory-service/src/main/java/com/microservices/inventory_service/config/RabbitMQConfig.java
@@ -1,5 +1,9 @@
-package com.microservices.inventory_service.config;
+package com.microservices.inventory_service.config; 
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -7,9 +11,31 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
-    
+
+    public static final String EXCHANGE_NAME = "ecommerce.events";
+    public static final String STOCK_QUEUE = "inventory.stock.queue";
+    public static final String STOCK_ROUTING_KEY = "stock.*";
+
     @Bean
-    public MessageConverter messageConverter() {
+    public Queue stockQueue() {
+        return new Queue(STOCK_QUEUE, true);
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(EXCHANGE_NAME);
+    }
+
+    @Bean
+    public Binding stockBinding(Queue stockQueue, TopicExchange exchange) {
+        return BindingBuilder
+                .bind(stockQueue)
+                .to(exchange)
+                .with(STOCK_ROUTING_KEY);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
         return new Jackson2JsonMessageConverter();
     }
 }
